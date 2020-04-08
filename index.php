d07c285a6cd45c257d2f5d8cd1672683d930b3db
Leo <bychkov.l47@mail.ru>
2020-04-08 17:01:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Аромасвечи</title>
+
+    <link rel="stylesheet" href="./view/resources/css/style.css">
+</head>
+<body>
+    <div class="container">
+
+        <div class="wrapper">
+            <header>
+                <nav>
+                    <a href="#" class="nav-items">СТРАНИЦА</a>
+                    <a href="#" class="nav-items">СТРАНИЦА</a>
+                    <a href="#" class="nav-items">СТРАНИЦА</a>
+                    <a href="#" class="nav-items">СТРАНИЦА</a>
+                </nav>
+                <button>Личный кабинет</button>
+            </header>
+
+            <main>
+                <div class="main-up-content">
+                    <div class="main-up-content left">
+                        <h2>СВЕЧИ ПО АКЦИИ</h2>
+                        <div class="candles">
+                            <div class="candles__candle">
+                              <a href="#" class="candle__src"></a>  
+                            </div>
+
+                            <div class="candles__candle">
+                              <a href="#" class="candle__src"></a>  
+                            </div>
+
+                            <div class="candles__candle">
+                              <a href="#" class="candle__src"></a>  
+                            </div>
+
+                            <div class="candles__candle">
+                              <a href="#" class="candle__src"></a>  
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="main-up-content left">
+                        <h2>ТОП СВЕЧЕЙ</h2>
+                        <div class="candles">
+                            <div class="candles__candle">
+                              <a href="#" class="candle__src"></a>  
+                            </div>
+
+                            <div class="candles__candle">
+                              <a href="#" class="candle__src"></a>  
+                            </div>
+
+                            <div class="candles__candle">
+                              <a href="#" class="candle__src"></a>  
+                            </div>
+
+                            <div class="candles__candle">
+                              <a href="#" class="candle__src"></a>  
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+
+    </div>
+</body>
+</html>
