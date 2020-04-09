7609670222d499eecd9f58b6f73abb57ca482583
Vladislav <PeinDefender@gmail.com>
2020-04-09 14:33:38 +0300

    Открытие/скрытие модального окна

--- controller/js/main.js
+++ controller/js/main.js
@@ -1,6 +1,16 @@
-document.querySelector('div.login-window').addEventListener('click', () => {
-    document.querySelector('div.modal-window').style.top = "55px";
-})
+let check = 0;
+document.querySelector('div.login-window').addEventListener('click', showAndHide);
+
+function showAndHide(){
+    if(!check){
+        document.querySelector('div.modal-window').style.top = "55px";
+        check = 1;
+    }
+    else {
+        document.querySelector('div.modal-window').style.top = "-150px";
+        check = 0;
+    }
+}
 
 document.querySelector('#select-candle-color').addEventListener('input', (e) => {
     document.querySelector('label.color-var.user').style.background = e.target.value;
