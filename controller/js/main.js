fd70932fe3d48e15b5451793a8943e46b934d443
Vladislav <PeinDefender@gmail.com>
2020-04-09 14:22:20 +0300

    Добавлен выбор цвета

--- controller/js/main.js
+++ controller/js/main.js
@@ -1,3 +1,7 @@
 document.querySelector('div.login-window').addEventListener('click', () => {
     document.querySelector('div.modal-window').style.top = "55px";
 })
+
+document.querySelector('#select-candle-color').addEventListener('input', (e) => {
+    document.querySelector('label.color-var.user').style.background = e.target.value;
+})
