e99ce4fe2470cb002ccc9749b5ff2352e4b44337
Vladislav <PeinDefender@gmail.com>
2020-04-10 14:05:14 +0300

    Регистрация

--- index.php
+++ index.php
@@ -19,9 +19,9 @@
       <?php include './model/modalWindow.html'?>
       <header>
           <nav>
-              <a href="#" class="nav-items">СТРАНИЦА</a>
-              <a href="#" class="nav-items">СТРАНИЦА</a>
-              <a href="#" class="nav-items">СТРАНИЦА</a>
+              <a href="#" class="nav-items">ГЛАВНАЯ</a>
+              <a href="#" class="nav-items">КОНСТРУКТОР</a>
+              <a href="#" class="nav-items">КАТАЛОГ </a>
               <a href="#" class="nav-items">СТРАНИЦА</a>
               <div class="login-window">
                 <button>Личный кабинет
@@ -32,6 +32,7 @@
                     <input type="text" placeholder="Введите Логин" id="loginAuthU" class="" name="loginAuthU" required autofocus>
                     <input type="password" placeholder="Введите Пароль" id="passwordAuthU" name="passwordAuthU" required>
                     <input type="submit" value="Войти" id="buttonAuth" name="buttonAuth">
+                    <a href="./view/resources/regPage.php">Зарегистрироваться</a>
                   </form>
                 </div>
               </div>
