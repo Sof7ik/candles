3216a97189df76a4a729ece62c769e23636b0102
Leonid <bychkov.l47@mail.ru>
2020-04-10 14:21:46 +0300

    new database

--- model/candles.sql
+++ model/candles.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Апр 09 2020 г., 14:52
--- Версия сервера: 5.6.41
--- Версия PHP: 5.5.38
+-- Время создания: Апр 10 2020 г., 14:21
+-- Версия сервера: 8.0.15
+-- Версия PHP: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,57 @@ SET time_zone = "+00:00";
 --
 -- База данных: `candles`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `candles`
+--
+
+CREATE TABLE `candles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `form_id` int(11) NOT NULL,
+  `color_id` int(11) NOT NULL,
+  `cost` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `colors`
+--
+
+CREATE TABLE `colors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hex` varchar(7) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `forms`
+--
+
+CREATE TABLE `forms` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `types`
+--
+
+CREATE TABLE `types` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -45,11 +96,38 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `login`, `name`, `surname`, `password`, `email`, `phone`) VALUES
 (1, 'userLogin1', 'userName1', 'userSurname1', 'userPassword1', 'userEmail1', '123456789'),
 (2, 'userLogin2', 'userName2', 'userSurname2', 'userPassword2', 'userEmail2', '123456789'),
-(3, '123', '123', '123', '123', '123@123', '123456789');
+(8, 'leo_bychkov', 'Леонид', 'Бычков', '123123', 'alekse-bychkov@mail.ru', '90543543543');
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `candles`
+--
+ALTER TABLE `candles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `type_id` (`type_id`,`form_id`,`color_id`),
+  ADD KEY `color_id` (`color_id`),
+  ADD KEY `form_id` (`form_id`);
+
+--
+-- Индексы таблицы `colors`
+--
+ALTER TABLE `colors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `forms`
+--
+ALTER TABLE `forms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `types`
+--
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `users`
@@ -62,10 +140,46 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `candles`
+--
+ALTER TABLE `candles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `colors`
+--
+ALTER TABLE `colors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `forms`
+--
+ALTER TABLE `forms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `types`
+--
+ALTER TABLE `types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `candles`
+--
+ALTER TABLE `candles`
+  ADD CONSTRAINT `candles_ibfk_1` FOREIGN KEY (`color_id`) REFERENCES `colors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `candles_ibfk_2` FOREIGN KEY (`form_id`) REFERENCES `forms` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `candles_ibfk_3` FOREIGN KEY (`type_id`) REFERENCES `types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
