da39bb364c255d972c80b44abf7a30381b5646ef
Leonid <bychkov.l47@mail.ru>
2020-04-15 18:01:59 +0300

    cookie on registration

--- controller/php/reg.php
+++ controller/php/reg.php
@@ -13,57 +13,100 @@
         $phone = $_POST['telU'];
 
         if($password == $repeatPassword){
-            $checkUser = mysqli_query($link, "SELECT * FROM `users` WHERE `email` = $mail;");
+            $password = password_hash($password, PASSWORD_DEFAULT);
+
+            $checkUser = mysqli_query($link, "SELECT * FROM `users` WHERE `email` = '$mail';");
+
             if ($checkUser)
             {
                 $checkUserResult = mysqli_num_rows($checkUser);
-                if($checkUserResult > 0)
+                if($checkUserResult == 0)
                 {
-                    echo "Пользователь с таким email уже существует!";
-                }
-            }
-            else
-            {
-                $query = mysqli_query($link, "INSERT INTO `users`
-                (`id`, `name`, `surname`, `password`, `email`, `phone`) VALUES 
-                (NULL, '$name', '$surname', '$password', '$mail', '$phone');");
-                echo $query;
+                    // echo "Надо бы юзера добвавить";
+                    $newUser = mysqli_query($link, "INSERT INTO `users`
+                    (`id`, `name`, `surname`, `password`, `email`, `phone`) VALUES 
+                    (NULL, '$name', '$surname', '$password', '$mail', '$phone');");
 
-                if ($query)
-                {
+                    if ($newUser)
+                    {
+                        $getNewUser = mysqli_query($link, "SELECT * FROM `users` ORDER BY `id` DESC LIMIT 1;");
+                        if($getNewUser)
+                        {   
+                            $getNewUserResult = mysqli_fetch_assoc($getNewUser);
+
+                            // echo "<pre>";
+                            //     print_r($getNewUserResult);
+                            // echo "</pre>";
+                            if (isset($_COOKIE['userInfo']))
+                            {
+                                setcookie(
+                                    'userInfo',
+                                    '',
+                                    time()-3600,
+                                    '/'
+                                );
+
+                                $cookieName = 'userInfo';
+                                $cookieValue= serialize($getNewUserResult);
+                                $expire = time()+604800;
+                                $path = '/';
+                                
+                                setcookie(
+                                    $cookieName,
+                                    $cookieValue,
+                                    $expire,
+                                    $path
+                                );
+
+                                header('Location: ../../index.php');
+                            }
+                            else{
+                                $cookieName = 'userInfo';
+                                $cookieValue= serialize($getNewUserResult);
+                                $expire = time()+604800;
+                                $path = '/';
+                                
+                                setcookie(
+                                    $cookieName,
+                                    $cookieValue,
+                                    $expire,
+                                    $path
+                                );
+
+                                header('Location: ../../index.php');
+                            }
+                        }
+                        else
+                        {
+                            die(mysqli_error($getNewUser));
+                        }
+                    }
+                    else
+                    {
+                        die(mysqli_error($query));
+                    }
                     
-                    $getNewUser = mysqli_query($link, "
-                        SELECT id, name, surname FROM `users` ORDER BY `id` DESC LIMIT 1;
-                    ");
-                    $getNewUserResult = mysqli_fetch_assoc($getNewUser);
-                    ?>
-                        <pre>
-                            <?
-                                print_r($getNewUserResult);
-                            ?>
-                        </pre>
-                    <?
+                    
                 }
-                else
-                {
-                    die(mysqli_error($query));
-                }
+                // else
+                // {
+                //     echo "Есть юзер";
+                // }
             }
             
         }
-    }
+        // else
+        // {
+        //     echo "Пароли не свопадают";
+        //     echo "<br>";
 
-        // $cookieName = 'userId';
-        // $cookieValue = 
-        // setcookie(
-        //     $name, 
-        //     $value, 
-        //     $expire, 
-        //     $path, 
-        //     $domain, 
-        //     $secure, 
-        //     $httponly
-        // );
-        // header('Location: ../../index.php');
+        //     echo $password;
+        //     echo "<br>";
+
+        //     echo $repeatPassword;
+        //     echo "<br>";
+        // }
+        
+    }
     
 ?>
