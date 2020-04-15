1a4c17406148bd7a0d4c164fca8507343d73b11c
Leonid <bychkov.l47@mail.ru>
2020-04-15 15:19:53 +0300

    absolutely new BUY SCRIPT

--- controller/php/buy.php
+++ controller/php/buy.php
@@ -1,24 +1,62 @@
 <?php
+    require_once 'connection.php';
 
-    echo "Куплено!";
-    echo "<br>";
+    // заберем цвета из базы, чтобы сравнить их с выбранным цветом
+    $getColors = mysqli_query($link, 
+    "
+    SELECT id, hex FROM `colors`;
+    ");
+    $getColorsResult = mysqli_fetch_all($getColors, MYSQLI_ASSOC);
 
-    echo $_POST['type__vars'];
-    echo "<br>";
-
-    echo $_POST['shape__vars'];
-    echo "<br>";
-
-    echo $_POST['color__vars'];
-    echo "<br>";
-    
+    // Если пользователь выбрал пункт "пользовательский цвет" в конфигураторе, то мы сравниваем все цвета из массива с выбранным
     if ($_POST['color__vars'] == 'Юзерная')
     {
-        echo $_POST['user-color'];
-        echo "<br>"; 
+        $color = $_POST['user-color'];
+
+        foreach ($getColorsResult as $key => $value)
+        {
+            if ($color == $value['hex'])
+            {
+                break;
+            }
+            else
+            {   
+                $newColor = mysqli_query($link, "
+                    INSERT INTO `colors` 
+                    (`id`, `name`, `hex`) VALUES
+                    (NULL, '', '$color');
+                ");
+                if($newColor)
+                {
+                    $getLastColor = mysqli_query($link, 
+                    "
+                        SELECT id FROM `colors` ORDER BY `id` DESC LIMIT 1;
+                    ");
+                    $getLastColorResult = mysqli_fetch_assoc($getLastColor);
+                    $color = $getLastColorResult['id'];
+                    echo $color;
+                }
+                else
+                {
+                    die("Ошибкка: " . mysqli_error($link));
+                }
+                break;
+                
+            }
+        }
     }
+    else
+    {
+        $color = $_POST['color__vars'];
+    }
+    
+    $type = $_POST['type__vars'];
+    $shape = $_POST['shape__vars'];
+    $quantity = $_POST['quantity'];
 
-    echo $_POST['submit-btn'];
-    echo "<br>";
-
+    $newCandle = mysqli_query($link, "
+        INSERT INTO `candles`
+        (`id`, `name`, `type_id`, `form_id`, `color_id`, `cost`) VALUES 
+        (NULL, 'Юзерная свеча 1', $type, $shape, $color, 1000)
+    ");
 ?>
