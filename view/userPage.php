e068a40e80718cccef8e08883922010e3f2ec7e4
Leonid <bychkov.l47@mail.ru>
2020-04-15 15:21:50 +0300

    some fixes

--- view/userPage.php
+++ view/userPage.php
@@ -6,6 +6,7 @@
     <title>userPage</title>
     <link rel="stylesheet" href="/view/resources/css/userPage.css">
     <link rel="stylesheet" href="/view/resources/css/header.css">
+
     <link rel="stylesheet" href="/view/resources/css/loginModalWindow.css">
     <link rel="stylesheet" href="/view/resources/css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
@@ -13,33 +14,40 @@
 <body>
     <div class="wrapper">
         <?php include 'header.html'; ?>
-        <div class='info'>
-            <div class='photo'>
-                <img src='/view/resources/img/pics/1.png'>
+        <div class="main-wrapper">
+            <div class="container">
+                <div class='info'>
+                    <div class='photo'>
+                        <img src='/view/resources/img/pics/1.png'>
+                    </div>
+                    <ul class='infoAboutUser'>
+                        <li>
+                            <span>Имя</span>
+                            <p>123</p>
+                        </li>
+                        <li>
+                            <span>Фамилия</span>
+                            <p>123</p>
+                        </li>
+                        <li>
+                            <span>Почта</span>
+                            <p>123@123</p>
+                        </li>
+                        <li>
+                            <span>Телефон</span>
+                            <p>+123456789</p>
+                        </li>
+                        <li>
+                            <span>Адрес</span>
+                            <p>123</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <ul class='infoAboutUser'>
-                <li>
-                    <span>Имя</span>
-                    <p>123</p>
-                </li>
-                <li>
-                    <span>Фамилия</span>
-                    <p>123</p>
-                </li>
-                <li>
-                    <span>Почта</span>
-                    <p>123@123</p>
-                </li>
-                <li>
-                    <span>Телефон</span>
-                    <p>+123456789</p>
-                </li>
-                <li>
-                    <span>Адрес</span>
-                    <p>123</p>
-                </li>
-            </ul>
+            
+
         </div>
+
     </div>
     <script src='/controller/js/login-window.js'></script>
 </body>
