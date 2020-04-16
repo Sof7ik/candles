308d83e5bc69aa6e8c4122152dac745477c8fa80
Leonid <bychkov.l47@mail.ru>
2020-04-16 11:51:50 +0300

    userPage adaptiver for COOKIE

---
+++ controller/js/userPage.js
@@ -0,0 +1,112 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    let oldInfoArray = [],
+    newInfoArray = [],
+    flag = false;
+
+    let elems = document.querySelectorAll('ul.infoAboutUser li p');
+
+    
+    elems.forEach((element) => {
+        oldInfoArray.push(element.textContent);
+    })
+
+    console.log('infoArray: ', oldInfoArray);
+
+    document.querySelector('button.edit-prof').addEventListener('click', () => {
+        if (flag == false)
+        {
+            flag = true;
+            console.log(flag);
+
+            document.querySelector('button.edit-prof').textContent = "Сохранить";
+
+            elems.forEach((element) => {
+                element.remove();
+            })
+
+            let i = 0;
+            document.querySelectorAll('ul.infoAboutUser li').forEach ((elem) => 
+            {
+                elem.insertAdjacentHTML('beforeend',
+                `
+                    <input type='text' value='${oldInfoArray[i]}' class="edit-input input-${i}">
+                `);
+                i++;
+            });
+
+            document.querySelector('input.edit-input.input-3').addEventListener('input', () => {
+
+                if (event.target.value != '0' || 
+                    event.target.value != '1' || 
+                    event.target.value != '2' || 
+                    event.target.value != '3' || 
+                    event.target.value != '4' || 
+                    event.target.value != '5' || 
+                    event.target.value != '6' || 
+                    event.target.value != '7' || 
+                    event.target.value != '8' || 
+                    event.target.value != '9' || 
+                    event.target.value != '+') 
+                {
+                    console.log("не цифра");
+                }
+
+                if(event.target.value.length > 12)
+                {
+                    event.target.value = '';
+                    console.log("value > 12");
+                }
+
+                console.log('event.target.value: ', event.target.value);
+                console.log('event.target.value.length: ', event.target.value.length);
+            })
+        }
+        else
+        {
+            flag = false;
+            document.querySelectorAll('ul.infoAboutUser li input').forEach ((elem) => {
+                newInfoArray.push(elem.value);
+            })
+            console.log(newInfoArray);
+
+            const getCookie = (name) => {
+                let matches = document.cookie.match(new RegExp(
+                "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+                ));
+                return matches ? decodeURIComponent(matches[1]) : undefined;
+            }
+
+            let newCookie = JSON.parse(getCookie('userInfo'));
+            let change = JSON.parse(getCookie('userInfo'));
+            console.log('newCookie: ', newCookie);
+
+            console.log('change["id"]: ', change['id']);
+            console.log('change["password"]: ', change['password']);
+
+            let counter = 0;
+            for(let key in newCookie)
+            {
+                if(newCookie.hasOwnProperty(key))
+                {
+                    console.log(key);
+                    console.log(newCookie[key]);
+                    if (key = 'id')
+                    {
+                        newCookie[key] = change['id'];
+                    }
+                    if (key = 'password')
+                    {
+                        newCookie[key] = change['password'];
+                    }
+                    newCookie[key] = newInfoArray[counter];
+                    counter++;
+                }
+            }
+
+            console.log("newCookie после замены", newCookie);
+
+            document.cookie = `userInfo=${JSON.stringify(newCookie)}; max-age=86400e3; path='/'`;
+        }
+        
+    })
+})
--- view/userPage.php
+++ view/userPage.php
@@ -20,36 +20,66 @@ if (!isset($_COOKIE['userInfo']))
 </head>
 <body>
     <div class="wrapper">
-        <?php include 'header.html'; ?>
+        <?php include 'header.php'; ?>
         <div class="main-wrapper">
             <div class="container">
                 <div class='info'>
-                    <div class='photo'>
-                        <img src='/view/resources/img/pics/1.png'>
+                    <div class="sub-nav">
+                        <button class="edit-prof">Изменить</button>
                     </div>
+                    <!-- <div class='photo'>
+                        <img src='/view/resources/img/pics/1.png'>
+                    </div> -->
                     <ul class='infoAboutUser'>
                         <li>
                             <span>Имя</span>
-                            <p>123</p>
+                            <p id="name"> 
+                                <?
+                                    // echo json_decode($_COOKIE['userInfo'])->name;
+                                    echo unserialize($_COOKIE['userInfo'])['name'];
+                                ?>
+                            </p>
                         </li>
                         <li>
                             <span>Фамилия</span>
-                            <p>123</p>
+                            <p id="surname">
+                                <?
+                                    // echo json_decode($_COOKIE['userInfo'])->surname;
+                                    echo unserialize($_COOKIE['userInfo'])['surname'];
+                                ?>
+                            </p>
                         </li>
                         <li>
                             <span>Почта</span>
-                            <p>123@123</p>
+                            <p id="email">
+                                <?
+                                    // echo json_decode($_COOKIE['userInfo'])->email; 
+                                    echo unserialize($_COOKIE['userInfo'])['email'];
+                                ?>
+                            </p>
                         </li>
                         <li>
                             <span>Телефон</span>
-                            <p>+123456789</p>
+                            <p id="phone">
+                                <?
+                                    // echo json_decode($_COOKIE['userInfo'])->phone;
+                                    echo unserialize($_COOKIE['userInfo'])['phone'];
+                                ?>
+                            </p>
                         </li>
                         <li>
                             <span>Адрес</span>
-                            <p>123</p>
+                            <p id="address">
+                                <?
+                                    // echo json_decode($_COOKIE['userInfo'])->address;
+                                    echo unserialize($_COOKIE['userInfo'])['address'];
+                                ?>
+                            </p>
                         </li>
                     </ul>
                 </div>
+                
+                
             </div>
             
 
@@ -57,5 +87,6 @@ if (!isset($_COOKIE['userInfo']))
 
     </div>
     <script src='/controller/js/login-window.js'></script>
+    <!-- <script src="/controller/js/userPage.js" defer></script> -->
 </body>
 </html>
