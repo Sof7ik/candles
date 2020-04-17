04e1e46ee88bfc2f041a9e9a7dd270ce844215e2
Vladislav <PeinDefender@gmail.com>
2020-04-17 17:41:12 +0300

    Нормальное закрытие левой модалки

--- controller/js/leftModalWindow.js
+++ controller/js/leftModalWindow.js
@@ -1,42 +1,43 @@
 let number1, number2;
 
-function hideModalOnClckClose () {
+function hideModalOnClickClose () {
     document.querySelector('div.leftModal').style.width = '0px';
     document.querySelectorAll('div.leftModal *').forEach((tmp) => {
         tmp.style.display = 'none';
     })
 }
 
+function showModal(){
+    document.querySelector('div.leftModal').style.width = '100px';
+    document.querySelectorAll('div.leftModal *').forEach((tmp) => {
+        tmp.style.display = "block";
+    })
+    number2 = number1;
+    number1 = undefined;
+}
+
+function hideModal(){
+    document.querySelector('div.leftModal').style.width = '0px';
+    document.querySelectorAll('div.leftModal *').forEach((tmp) => {
+        tmp.style.display = 'none';
+    })
+    
+}
+
 document.querySelectorAll('.candles__candle').forEach((e) => {
     e.addEventListener('click', (event) => {
-        // number1 = event.target.dataset.candlenumber;
-        // console.log('number1: ', number1);
-        // number2 = event.target.dataset.candlenumber;
-        // console.log('number2: ', number2);
-
-        if(number1 == number2)
-        {
-            console.log("совпадают");
+        number1 = event.target.dataset.candlenumber;
+        if(document.querySelector('div.leftModal').style.width != "100px"){
+            showModal();
+        }
+        if(number1 == number2){
+            hideModal();
+        }
+        if(number1 != number2 && number1 != undefined){
+            number2 = number1;
+            document.querySelector('div.leftModal .modal-title p').textContent = 'изменено' + number2;
         }
 
-        if(document.querySelector('div.leftModal').style.width != "100px")
-        {
-            document.querySelector('div.leftModal').style.width = '100px';
-            document.querySelectorAll('div.leftModal *').forEach((elem) => {
-                elem.style.display = "block";
-            })
-            document.querySelector('div.leftModal .modal-title *').style.display = "block";
-        }
-        else
-        {
-            document.querySelector('div.leftModal').style.width = '0px';
-            document.querySelectorAll('div.leftModal *').forEach((tmp) => {
-                tmp.style.display = 'none';
-            })
-        }
-        console.log(event.target);
-        
-        
-        document.querySelector('div.leftModal .modal-title p').addEventListener('click', hideModalOnClckClose)
+        document.querySelector('div.leftModal .modal-title p').addEventListener('click', hideModalOnClickClose);
     })
 })
