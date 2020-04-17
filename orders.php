ef8bd2388b02c35c25a2b4e534b53cb57ce14e0a
Leonid <bychkov.l47@mail.ru>
2020-04-17 18:37:46 +0300

    orders page pre-final

--- orders.php
+++ orders.php
@@ -22,10 +22,31 @@
         <div class="container">
             <div class="main-wrapper">
                 <span>Все Ваши Заказы</span>
+
+                <div class="grid-container">
+
+                    <div class="grid-element">
+                        <div class="item">
+                            <img src="/view/resources/img/pics/1.png" alt="candle" class="order-candle">
+
+                            <div class="texts">
+                                <p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor</p>
+                                <span>В стакане + с закругленным дном + красная</span>
+                                <span class="item-price">2 * 1000 руб. = 2000 руб.</span>
+                            </div>
+                        </div>
+
+                        <div class="item-buttons">
+                            <button class="item-buttons__buton">Купить снова</button>
+                            <button class="item-buttons__buton">Посмотреть детали</button>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </main>
 
-
+    <script src="controller/js/login-window.js"></script>
 </body>
 </html>
