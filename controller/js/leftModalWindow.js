d5dc4b21d523952eae6bbc01462814da729a6a99
Vladislav <PeinDefender@gmail.com>
2020-04-17 21:22:14 +0300

    Оптимизация левой модалки

--- controller/js/leftModalWindow.js
+++ controller/js/leftModalWindow.js
@@ -1,5 +1,8 @@
 let number1, number2;
 
+document.querySelector('div.leftModal').addEventListener('mouseover', styleModal1);
+document.querySelector('div.leftModal').addEventListener('mouseout', styleModal2);
+
 function hideModalOnClickClose () {
     document.querySelector('div.leftModal').style.width = '0px';
     document.querySelectorAll('div.leftModal *').forEach((tmp) => {
@@ -10,7 +13,7 @@ function hideModalOnClickClose () {
 function showModal(){
     document.querySelector('div.leftModal').style.width = '100px';
     document.querySelectorAll('div.leftModal *').forEach((tmp) => {
-        tmp.style.display = "block";
+        tmp.style.display = "flex";
     })
     number2 = number1;
     number1 = undefined;
@@ -21,7 +24,6 @@ function hideModal(){
     document.querySelectorAll('div.leftModal *').forEach((tmp) => {
         tmp.style.display = 'none';
     })
-    
 }
 
 document.querySelectorAll('.candles__candle').forEach((e) => {
@@ -35,9 +37,17 @@ document.querySelectorAll('.candles__candle').forEach((e) => {
         }
         if(number1 != number2 && number1 != undefined){
             number2 = number1;
-            document.querySelector('div.leftModal .modal-title p').textContent = 'изменено' + number2;
+            document.querySelector('div.leftModal .modal-title .closeLeftModal').textContent = 'изменено' + number2;
         }
 
-        document.querySelector('div.leftModal .modal-title p').addEventListener('click', hideModalOnClickClose);
+        document.querySelector('div.leftModal .modal-title .closeLeftModal').addEventListener('click', hideModalOnClickClose);
     })
 })
+
+function styleModal1(){
+    document.querySelector('div.leftModal').style.width='max-content';
+}
+
+function styleModal2(){
+    document.querySelector('div.leftModal').style.width='100px';
+}
