a4cf4f39f3cfa2d515a8ba82ef49ac6f13325a0d
Leonid <bychkov.l47@mail.ru>
2020-04-18 23:21:37 +0300

    visual fix

--- forgot.php
+++ forgot.php
@@ -6,8 +6,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Восстановление пароля</title>
+
+    <link rel="stylesheet" href="view/resources/css/style.css">
+    <link rel="stylesheet" href="view/resources/css/header.css">
+    <link rel="stylesheet" href="view/resources/css/loginModalWindow.css">
 </head>
 <body>
+    <?include __DIR__ . '/view/header.php'?>
     <p>не готово, но скоро-скоро</p>
 </body>
 </html>
